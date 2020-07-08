0ba2e1a80c9981e86923264abab50f2a50b9eafe
Eduardo Augusto Coleta de Souza <souzaeduardoac@debian.XU555B>
2020-07-08 20:07:37 -0300

    Added Repository Pattern example

--- src/main/java/org/acme/getting/started/FruitResource.java
+++ src/main/java/org/acme/getting/started/FruitResource.java
@@ -1,7 +1,5 @@
 package org.acme.getting.started;
 
-import org.acme.getting.started.Fruit;
-
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -9,11 +7,19 @@ import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/fruit")
-public class FuitResource {
+public class FruitResource {
+
+    FruitRepository fruitRepository;
+    public FruitResource(FruitRepository fruitRepository) {
+        this.fruitRepository = fruitRepository;
+    }
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Fruit> fruits(@QueryParam("season") String season) {
-        if (season != null) return Fruit.findBySeason(season);
+        if (season != null)
+            return fruitRepository.findBySeason(season);
+            // return Fruit.findBySeason(season); Active Repository Pattern
         return Fruit.listAll();
     }
 
